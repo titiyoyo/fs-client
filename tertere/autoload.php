8488d08bcf7b52af87e6b2c1c303a73918693243
Terence Pires <terence.pires@gmail.com>
2016-12-07 11:21:48 +0100

    test

--- tertere/autoload.php
+++ tertere/autoload.php
@@ -2,18 +2,11 @@
 
 spl_autoload_register(
     function() {
-        include_once realpath('vendor/tertere/Tertere/Fs/ClientBase.php');
-        include_once realpath('vendor/tertere/Tertere/Fs/ItemBase.php');
-        include_once realpath('vendor/tertere/Tertere/Utilities.php');
-        include_once realpath('vendor/tertere/Tertere/Fs/Ftp/FtpClient.php');
-        include_once realpath('vendor/tertere/Tertere/Fs/Ftp/FtpItem.php');
-        include_once realpath('vendor/tertere/Tertere/Fs/Local/LocalClient.php');
-
-     //    include_once 'Tertere/Fs/ClientBase.php';
-     //    include_once 'Tertere/Fs/ItemBase.php';
-    	// include_once 'Tertere/Utilities.php';
-    	// include_once 'Tertere/Fs/Ftp/FtpClient.php';
-    	// include_once 'Tertere/Fs/Ftp/FtpItem.php';
-     //    include_once 'Tertere/Fs/Local/LocalClient.php';
+        include_once realpath('fsclient/Tertere/Fs/ClientBase.php');
+        include_once realpath('fsclient/Tertere/Fs/ItemBase.php');
+        include_once realpath('fsclient/Tertere/Utilities.php');
+        include_once realpath('fsclient/Tertere/Fs/Ftp/FtpClient.php');
+        include_once realpath('fsclient/Tertere/Fs/Ftp/FtpItem.php');
+        include_once realpath('fsclient/Tertere/Fs/Local/LocalClient.php');
     }
 );
