0303dc03370a2872a48175a4497f9f016bb60442
Terence Pires <terence.pires@gmail.com>
2017-03-20 07:30:14 +0100

    class autoload update

--- autoload.php
+++ autoload.php
@@ -2,11 +2,13 @@
 
 spl_autoload_register(
     function() {
-        include_once realpath('src/Fs/ClientBase.php');
-        include_once realpath('src/Fs/ItemBase.php');
-        include_once realpath('src/Utilities.php');
-        include_once realpath('src/Fs/Ftp/FtpClient.php');
-        include_once realpath('src/Fs/Ftp/FtpItem.php');
-        include_once realpath('src/Fs/Local/LocalClient.php');
+    	include_once realpath('./src/Fs/FsClientLogger.php');
+		include_once realpath('./src/Fs/ItemBase.php');
+        include_once realpath('./src/Fs/FsClientBase.php');
+        include_once realpath('./src/Utilities.php');
+        include_once realpath('./src/Fs/Ftp/FtpClient.php');
+        include_once realpath('./src/Fs/Ftp/FtpItem.php');
+        include_once realpath('./src/Fs/Local/LocalClient.php');
+        include_once realpath('./src/Fs/Local/LocalItem.php');
     }
 );
