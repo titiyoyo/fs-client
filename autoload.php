b5d66dd3dcac2a2fe3b9ca81f25eb1cc8c6003fc
Terence Pires <terence.pires@gmail.com>
2017-04-02 14:05:57 +0200

    autoload refactored

--- autoload.php
+++ autoload.php
@@ -2,13 +2,18 @@
 
 spl_autoload_register(
     function() {
-    	include_once realpath('./src/Fs/FsClientLogger.php');
-		include_once realpath('./src/Fs/ItemBase.php');
-        include_once realpath('./src/Fs/FsClientBase.php');
-        include_once realpath('./src/Utilities.php');
-        include_once realpath('./src/Fs/Ftp/FtpClient.php');
-        include_once realpath('./src/Fs/Ftp/FtpItem.php');
-        include_once realpath('./src/Fs/Local/LocalClient.php');
-        include_once realpath('./src/Fs/Local/LocalItem.php');
+		include_once realpath('./src/FsClient/Clients/ItemBase.php');
+		include_once realpath('./src/FsClient/Clients/FsClientBase.php');
+		include_once realpath('./src/FsClient/Clients/Ftp/FtpItem.php');
+        include_once realpath('./src/FsClient/Clients/Ftp/FtpClient.php');
+        include_once realpath('./src/FsClient/Clients/Local/LocalItem.php');
+        include_once realpath('./src/FsClient/Clients/Local/LocalClient.php');
+        include_once realpath('./src/FsClient/User/FsUser.php');
+        include_once realpath('./src/FsClient/User/UserRight.php');
+        include_once realpath('./src/FsClient/Manager.php');
+        include_once realpath('./src/Utilities/Http.php');
+        include_once realpath('./src/Utilities/Logger.php');
+        include_once realpath('./src/Utilities/MimeTypes.php');
+        include_once realpath('./src/Utilities/Validation.php');
     }
 );
